24f722d49dc90f81444ed672406e3c47c6d5bc71
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-06 21:19:18 -0700

    Setup puppeteer-extra and stealth plugin
    Need to get around cloudflare for scraping

--- js/scrape.js
+++ js/scrape.js
@@ -1,22 +1,27 @@
 // Dependencies
 const axios = require("axios"); // HTTP Client
 const cheerio = require("cheerio"); // HTML parser, good for static pages
-const puppeteer = require("puppeteer"); // Headless Browser, good for dynamic pages
+const puppeteer = require("puppeteer-extra"); // Headless Browser, good for dynamic pages
+const stealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(stealthPlugin());
 
 console.log("Scrape has been run");
 
 async function getTest() {
-    // Fetch a page with axios
-    const { data: html } = await axios.get('https://example.com');
 
-    // Load into Cheerio
-    const $ = cheerio.load(html);
-    console.log('Page title:', $('title').text());
+    const url = "https://www.kobo.com/us/en/ebook/howling-dark-4";
 
-    // Launch Puppeteer to automate a browser
+    // Setup for puppeteer
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+
+    // Navitage to the page and wait till the network is idle
+    const options = {waitUntil: "networkidle2"}; // can try 2 also
+    await page.goto(url, options);
+
+    // Wait for the cover image to load
+    // await page.waitForSelector("img.cover-image");
+
     console.log('Puppeteer page title:', await page.title());
     await browser.close();
 }
