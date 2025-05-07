265e1545622df4679b019c8774190f72ef97dc0a
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-06 21:41:05 -0700

    Can obtain correct full resolution img url from kobo
    Able to extract and fix the url for cover images

--- js/scrape.js
+++ js/scrape.js
@@ -7,23 +7,40 @@ puppeteer.use(stealthPlugin());
 
 console.log("Scrape has been run");
 
-async function getTest() {
+async function getKoboCoverImg() {
 
-    const url = "https://www.kobo.com/us/en/ebook/howling-dark-4";
+    // the -# here gives different books of this name(or editions), sometimes there is no book/page relating to it
+    // in that case I need to have a behavior/timeout. I think I should iterate through a few pages
+    // based on the name and then display image results to the viewer
+    // sometimes for unique book names with only 1 edition there is no -#
+    const url = "https://www.kobo.com/us/en/ebook/the-blade-itself-4";
 
     // Setup for puppeteer
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless:true,
+    });
     const page = await browser.newPage();
 
     // Navitage to the page and wait till the network is idle
-    const options = {waitUntil: "networkidle2"}; // can try 2 also
+    const options = {waitUntil: "networkidle2"}; // can try 0 also
     await page.goto(url, options);
 
     // Wait for the cover image to load
-    // await page.waitForSelector("img.cover-image");
+    const elementImg = "img.cover-image"
+    await page.waitForSelector(elementImg);
 
-    console.log('Puppeteer page title:', await page.title());
+    // Extract the src attribute of the cover image
+    const coverUrl = await page.$eval(elementImg, img => img.src);
+    const splitUrl = coverUrl.split("/");
+    // Remove parts of the URL that resize the image
+    const startIndex = -5;
+    const deleteItems = 4;
+    splitUrl.splice(startIndex, deleteItems);
+    const fullResUrl = splitUrl.join("/");
+    console.log(fullResUrl);
+     
+
     await browser.close();
 }
 
-getTest();
+getKoboCoverImg();
